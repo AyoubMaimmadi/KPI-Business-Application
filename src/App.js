336ccb26169a3a54c182a1186f7941c610790faf
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-20 15:58:04 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 
 const App = () => {
-  return <h1 className="underline text-3xl">App</h1>
+  return <h1 className="">App</h1>
 }
 
 export default App
