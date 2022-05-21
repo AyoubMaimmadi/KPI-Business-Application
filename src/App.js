4a64c29cfbfae95d968ab1a50918a96b7b86685b
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-21 19:36:04 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,44 +1,29 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
-import { FiSettings } from 'react-icons/fi'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { FiSettings } from "react-icons/fi";
 
-import './App.css'
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from "./pages";
+
+import "./App.css";
 
 const App = () => {
-  const activeMenu = true
+  const activeMenu = true;
   return (
     <div>
       <Router>
         <div className="flex relativ dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000 ' }}>
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000 " }}>
             <TooltipComponent content="Settings" position="Top">
-              <button
-                type="button"
-                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                style={{ background: 'blue', borderRadius: '50%' }}
-              >
+              <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{ background: "blue", borderRadius: "50%" }}>
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
-          {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg">
-              sidbar
-            </div>
-          ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">sidbar w-0</div>
-          )}
-          <div
-            className={
-              activeMenu
-                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
-            }
-          >
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              navbar
-            </div>
+          {activeMenu ? <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg">sidbar</div> : <div className="w-0 dark:bg-secondary-dark-bg">sidbar w-0</div>}
+          <div className={activeMenu ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full" : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2"}>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">navbar</div>
           </div>
           <div>
             <Routes>
@@ -68,7 +53,7 @@ const App = () => {
         </div>
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
