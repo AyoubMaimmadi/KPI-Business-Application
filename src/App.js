5aee4e5f0d8a71585831875b82b57077ba3d8dd3
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-21 18:30:51 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import './App.css'
 
 const App = () => {
-  return <h1 className="">App</h1>
+  return (
+    <div>
+      <Router>
+        <div className="flex relativ"></div>
+      </Router>
+    </div>
+  )
 }
 
 export default App
