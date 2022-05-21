42e3a901304f4319ec331fb6b25f35703a647137
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-21 18:33:50 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 import './App.css'
 
@@ -7,7 +8,12 @@ const App = () => {
   return (
     <div>
       <Router>
-        <div className="flex relativ"></div>
+        <div className="flex relativ dark:bg-main-dark-bg">
+          <div
+            className="fixed right-4 bottom-4"
+            style={{ zIndex: '1000 ' }}
+          ></div>
+        </div>
       </Router>
     </div>
   )
