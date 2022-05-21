553ce477c2bdd7537117cab8cb069a44df380be8
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-21 18:35:33 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import { FiSettings } from 'react-icons/fi'
 
 import './App.css'
 
@@ -9,10 +10,13 @@ const App = () => {
     <div>
       <Router>
         <div className="flex relativ dark:bg-main-dark-bg">
-          <div
-            className="fixed right-4 bottom-4"
-            style={{ zIndex: '1000 ' }}
-          ></div>
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000 ' }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
         </div>
       </Router>
     </div>
