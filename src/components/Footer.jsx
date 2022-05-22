eb7b63b47424d70c695c221ce326c71432780724
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-22 23:20:35 +0100

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+const date = new Date().getFullYear()
+
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      © 2022 All rights reserved by NiGhtte.com
+      © NiGhtte {date}. All rights reserved.
     </p>
   </div>
 )
